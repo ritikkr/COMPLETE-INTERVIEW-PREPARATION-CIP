295328ef68183c4b10de109aeed5b402898bcff6
Ritik Kumar <ritikkumar2405199@gmail.com>
2021-05-01 08:59:14 +0530

    iterator notes added

---
+++ STL Overview C++/iterator_advance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10, 20, 30, 40, 50};
+    vector<int> :: iterator i = v.begin();
+    advance(i, 3);
+    cout<<(*i)<<endl;
+    return 0;
+}
+
+// advance moves i to n pos ahead
+// next returns the iterator to n position ahead whereas advance itself moves to that position.
