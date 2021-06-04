97eea2731212db090fe6a7fc718094595723d44e
Ritik Kumar <ritikkumar2405199@gmail.com>
2021-06-04 16:14:16 +0530

    some codechef solution added that might be deleted later

---
+++ Data Structures ( Basics )/Dequeue/dequeArray.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+struct Deque {
+    int size, cap;
+    int *arr;
+    Deque(int c)
+    {
+        size = 0;
+        cap = c;
+        arr = new int[c];
+    }
+    bool isFull() { return (cap == size); }
+    bool isEmpty() { return (size == 0); }
+
+    void insertFront(int x)
+    {
+        if(isFull()) { return; }
+        for(int i = size-1; i>=0; i++)
+        {
+            arr[i+1] = arr[i];
+        }
+        arr[0] = x;
+        size++;
+    }
+    void deleteFront() {
+        if(isEmpty()){ return ;}
+        for(int i = 0; i<size;i++)
+        {
+            arr[i] = arr[i+1];
+        }
+        size--;
+    }
+    int getFront()
+    {
+        if(isEmpty()) { return -1; }
+        else return arr[0];
+    }
+    void insertRear(int x) {
+        if(isFull()) return;
+        arr[size] = x;
+        size++;       
+    }
+
+    void deleteRear() {
+        if(isEmpty()){ return; }
+        size--;
+    }
+
+    int getRear() {
+        if(isEmpty()) { return; }
+        else return arr[size-1];
+    }
+};
+int m   ain()
+{
+    
+
+    return 0;
+}
